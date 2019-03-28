2c4cc6c4aabd616b960140d5c17bc0e1bbb9ba20
Anliec <nicolas.six@pau.fr>
2019-03-28 18:16:43 -0400

    update q1

--- hw4/Q1/pagerank.py
+++ hw4/Q1/pagerank.py
@@ -28,14 +28,15 @@ Your algorithm, if implemented correctly, should return the same scores
 as popular graph analysis libraries like NetworkX.
 """
 def pagerank(index_file, edge_file, max_node_id, edge_count, damping_factor=0.85, iterations=10):
+    import os
     index_map = mmap.mmap(
         index_file.fileno(),
-        length=99999,  # ~~~ MODIFY THIS LINE (i) ~~~
+        length=os.fstat(index_file.fileno()).st_size,  # ~~~ MODIFY THIS LINE (i) ~~~
         access=mmap.ACCESS_READ)
 
     edge_map = mmap.mmap(
         edge_file.fileno(),
-        length=99999,  # ~~~ MODIFY THIS LINE (ii) ~~~
+        length=os.fstat(edge_file.fileno()).st_size,  # ~~~ MODIFY THIS LINE (ii) ~~~
         access=mmap.ACCESS_READ)
 
     scores = [1.0 / (max_node_id + 1)] * (max_node_id + 1)
@@ -47,13 +48,13 @@ def pagerank(index_file, edge_file, max_node_id, edge_count, damping_factor=0.85
 
         for i in range(edge_count):
             source, target = unpack(
-                '??',  # ~~~ MODIFY THIS LINE (iii) ~~~
-                edge_map[i * 99999: i * 99999 + 99999])  # ~~~ MODIFY THIS LINE (iv) ~~~
+                '>ii',  # ~~~ MODIFY THIS LINE (iii) ~~~
+                edge_map[i * 8: i * 8 + 8])  # ~~~ MODIFY THIS LINE (iv) ~~~
 
             source_degree = unpack(
-                '??',  # ~~~ MODIFY THIS LINE (v) ~~~
-                index_map[source * 99999: source * 99999 + 99999])[1]  # ~~~ MODIFY THIS LINE (vi) ~~~
-
+                '<ii',  # ~~~ MODIFY THIS LINE (v) ~~~
+                index_map[source * 8: source * 8 + 8])[1]  # ~~~ MODIFY THIS LINE (vi) ~~~
+            print(len(new_scores), len(scores), target, source)
             new_scores[target] += damping_factor * scores[source] / source_degree
 
         min_pr = (1 - damping_factor) / (max_node_id + 1)
